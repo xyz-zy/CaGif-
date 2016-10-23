823fe8ffde59db56fc865206d2d875df3891251c
snn88 <sm.nn88@gmail.com>
2016-10-22 23:07:09 -0500

    modified inventory

--- js/Home.js
+++ js/Home.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator, Text, View, TouchableHighlight, Image, ScrollView } from 'react-native';
+import { AppRegistry, Navigator, Text, View, TouchableHighlight, Image, ScrollView, ListView } from 'react-native';
+
+  class Sprite extends Component {
+    constructor(props) {
+      super(props);
+
+      // Toggle the state every second
+      setInterval(() => {
+        this.setState({ health: health - 1, fullness: fullness - 1, happiness: happiness - 1});
+      }, 3000);
+    }
+  }
 
 export default class Home extends Component{
 
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+     dataSource: ds.cloneWithRows([<Image source={require('../graphics/hunger0.png')}></Image>,<Image source={require('../graphics/hunger0.png')}></Image>])
+    };
+    // this.healthStats = {health: 100, fullness: 100, happiness: 100};
+    // setInterval(() => {
+    //     this.healthStats({ health: this.healthStats.health - 1, fullness: this.healthStats.fullness - 1, happiness: this.healthStats.happiness - 1});
+    //   }, 3000);
+  }
+
   render() {
+    //let display = (this.healthStats[0] == 100) ? '../graphics/hunger0.png' : '../graphics/hunger3.png';
+          //     <View>
+          // <Image source = {display} style = {{width: 50, height: 50}}></Image>
+          // </View>
+    this.state.dataSource[0]
     return (
-        <Image 
+      <Image 
           source = {require('../graphics/background.png')}
           style = {{flex: 1, width: null, height: null}}>
+
           {this.renderButtons()}
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => <Text>{rowData}</Text>}
+          />
       </Image>
     );
   }
--- js/Inventory.js
+++ js/Inventory.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator, Text, View, TouchableHighlight } from 'react-native';
+import { AppRegistry, Navigator, Text, View, TouchableHighlight, Image, ScrollView, StyleSheet} from 'react-native';
 
 export default class Inventory extends Component{
   render() {
     return (
+        <Image 
+          source = {require('../graphics/InventoryBackground.jpg')}
+          style = {{flex: 1, width: null, height: null}}>
+          {this.renderBackButton()}
+          {this.renderFood()}
+        </Image>
+    );
+  }
+
+  renderBackButton() {
+    return(
       <View>
         <Text> Inventory </Text>
           <TouchableHighlight onPress= {() => {
@@ -14,6 +25,32 @@ export default class Inventory extends Component{
             </Text>
           </TouchableHighlight>
       </View>
+    );
+  }
+
+  renderFood() {
+    const styles = StyleSheet.create({ contentContainer: { justifyContent: 'center' } });
+    return(
+/*    <View>
+      <ScrollView horizontal = {true}/>
+        <Image 
+          source = {require('../graphics/InventoryBackground.jpg')}>
+        </Image>
+      </ScrollView>
+    </View>
+*/
+    <ScrollView horizontal = {true}>
+    <View style = {{justifyContent: 'center'}}>
+        <Image 
+          source = {require('../graphics/canFish.png')}
+          style = {{height : 83, width :83}}>
+        </Image>
+        <Image 
+          source = {require('../graphics/foodBowl2.png')}
+          style = {{height : 83, width :83}}>
+        </Image>
+        </View>
+      </ScrollView>
     );
   }
 }
