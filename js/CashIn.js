8a549fc65b5a84a128cb4c5cfbc1eda1e5cbbfa2
Michail Shaposhnikov <misha.shapo98@gmail.com>
2016-10-23 00:26:51 -0500

    Added HealthKit data
    Implemented getting stepCount from HealthKit and displaying it to the
    CashIn View

--- js/CashIn.js
+++ js/CashIn.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator, Text, View, TouchableHighlight } from 'react-native';
+import { AppRegistry, Navigator, Text, View, TouchableHighlight, AysncStorage } from 'react-native';
+import AppleHealthKit from 'react-native-apple-healthkit';
+ 
+
+// get the available permissions from AppleHealthKit.Constants object 
+const PERMS = AppleHealthKit.Constants.Permissions;
 
 export default class CashIn extends Component{
+  constructor(props){
+    super(props);
+    
+    this.state = {steps: "None"};
+    
+    let options = {
+      permissions: {
+        read: ["StepCount"]
+      }
+    }
+    let lastStepTimeStamp = -1;
+    let lastNumSteps = -1;
+    let currentNumSteps = -1;
+    AppleHealthKit.initHealthKit(options: Object, (err: Object, res: Object) => {
+    if(err) {
+        this.setState({steps: "Error Initializing HealthKit"});
+        return;
+    }
+    // healthkit initialized... 
+    let lastTimeStamp = this.getKeyFromStorage('HealthData:lastTimestamp');
+    if(lastTimeStamp == undefined){
+      lastTimeStamp = (new Date()).toISOString();
+      this.setKeyToStorage('HealthData:lastTimestamp',curTimeStamp);
+    }
+    let aggregate = this.getKeyFromStorage("HealthData:aggregate");
+    if(aggregate == undefined){
+      aggregate = 0;
+      this.setKeyToStorage("HealthData:aggregate",aggregate);
+    }
+    let now = (new Date()).toISOString();
+    aggregate = aggregate + this.getStepCountsBetween(lastTimeStamp,now);
+    lastTimeStamp = now;
+    this.setKeyToStorage("HealthData:aggregate",aggregate);
+    this.setKeyToStorage('HealthData:lastTimestamp', lastTimeStamp);
+});
+  }
   render() {
     return (
-      <View>
+      <View style= {{}}>
         <Text> CashIn </Text>
+        <Text></Text>
           <TouchableHighlight onPress= {() => {
               this.props.navigator.pop();
             }}>
@@ -16,4 +58,40 @@ export default class CashIn extends Component{
       </View>
     );
   }
+  getKeyFromStorage(key){
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null){
+        return value;
+      } else {
+        return undefined;
+      }
+    } catch (error) {
+      this.setState({steps: "Error reading " + key + " from storage."})
+    }
+  }
+
+  setKeyToStorage(key,value){
+    try {
+      await AsyncStorage.setItem(key,value);
+    } catch (error) {
+      this.setState({steps: "Error setting " + key + "to storage."});
+    }
+  }
+
+  getStepCountsBetween(startTS, endTS){
+    if(!startTS){
+      return;
+    }
+    let newDate;
+    if(!endTS){
+      endTS = (new Date()).toISOString();
+    }
+    let options = {
+      startDate: startTS,  // required 
+      endDate:   endTS         // optional; default now 
+    };
+  }
+
+
 }
