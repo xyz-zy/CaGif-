d3b7003927a22a626411162f36855993d7d89d3e
Elizabeth Rego <liz@Elizabeths-MacBook-Pro.local>
2016-10-23 00:20:44 -0500

    INventory buttons now chchange pet stats

--- js/Inventory.js
+++ js/Inventory.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { AppRegistry, Navigator, Text, View, TouchableHighlight, Image, ScrollView, StyleSheet} from 'react-native';
+import React, { Component} from 'react';
+import { AppRegistry, Navigator, Text, View, TouchableHighlight, Image, ScrollView, StyleSheet, AsyncStorage} from 'react-native';
 
 export default class Inventory extends Component{
   render() {
@@ -38,19 +38,62 @@ export default class Inventory extends Component{
         </Image>
       </ScrollView>
     </View>
-*/
-    <ScrollView horizontal = {true}>
-    <View style = {{justifyContent: 'center'}}>
-        <Image 
-          source = {require('../graphics/canFish.png')}
-          style = {{height : 83, width :83}}>
-        </Image>
-        <Image 
-          source = {require('../graphics/foodBowl2.png')}
-          style = {{height : 83, width :83}}>
-        </Image>
-        </View>
+ <Image
+ source = {require('../graphics/canFish.png')}
+ style = {{height : 83, width :83}}>
+ </Image>
+ */
+           <View marginTop = {125} marginBottom = {150} flex = {1} flexDirection = 'column' justifyContent = 'space-around'>
+           <ScrollView horizontal = {true} justifyContent = 'center' maxHeight = {83}>
+           <TouchableHighlight onPress={() => {
+           AsyncStorage.setItem('PetData:hunger', this.incrementHunger(20))
+           }}>
+           <Image
+            style={{width: 83, height: 83}}
+            source={require('../graphics/canFish.png')}>
+           </Image>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => {
+           AsyncStorage.setItem('PetData:hunger', this.incrementHunger(20))
+           }}>
+           <Image
+           style={{width: 83, height: 83}}
+           source={require('../graphics/foodBowl2.png')}>
+           </Image>
+        </TouchableHighlight>
       </ScrollView>
+           <ScrollView horizontal = {true} justifyContent = 'center' maxHeight = {83}>
+           <TouchableHighlight onPress={() => {
+           AsyncStorage.setItem('PetData:happiness', this.incrementHappiness(20))
+           }}>
+           <Image
+           style={{width: 83, height: 83}}
+           source={require('../graphics/yarn.jpg')}>
+           </Image>
+           </TouchableHighlight>
+           <TouchableHighlight onPress={() => {
+           AsyncStorage.setItem('PetData:happiness', this.incrementHappiness(20))
+           }}>
+           <Image
+           style={{width: 83, height: 83}}
+           source={require('../graphics/mouse.jpg')}>
+           </Image>
+           </TouchableHighlight>
+        </ScrollView>
+</View>
     );
   }
+    
+    
+    incrementHunger(hungerPoints){
+        let current = parseInt(AsyncStorage.getItem('PetData:hunger'));
+        current  = current + parseInt(hungerPoints);
+        return current.toString();
+    }
+    
+    incrementHappiness(happinessPoints){
+        let current = parseInt(AsyncStorage.getItem('PetData:happiness'));
+        current  = current + parseInt(happinessPoints);
+        return current.toString();
+    }
 }
